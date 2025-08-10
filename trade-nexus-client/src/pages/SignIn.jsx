9f3206506c55ca761169f0c053d89a56910a4a00
MdMubashirulAhsan <ahsan.mubashirul@gmail.com>
2025-08-10 19:59:03 +0600

    added 5 routes in navbar

--- trade-nexus-client/src/pages/SignIn.jsx
+++ trade-nexus-client/src/pages/SignIn.jsx
@@ -8,7 +8,7 @@ import useAuth from "../hooks/useAuth";
 import axios from "axios";
 import login from "../assets/lotties/login.json";
 import Lottie from "lottie-react";
-
+// lksdjflaksdjf
 const SignIn = () => {
   const { signInUser } = useAuth();
 const location = useLocation();
